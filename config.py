8ee8c266cf4907ea4b5ebce17eeb4b8000917590
Jeremy <o9I8U7Y6>
2014-06-26 20:16:25 +0800

    XY

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ import os.path
 
 ROOT_DIR=os.path.dirname(__file__)
 
-_DBUSER = "mduser"
-_DBPASS = "mduser"
+_DBUSER = "ibeaconuser"
+_DBPASS = "ibeacon"
 _DBNAME = "medicaldb"
-_DBHOST = "airui313.vicp.net:9906"
+_DBHOST = "114.215.179.6"
 
 ROOT_DIR=os.path.dirname(__file__)
 
